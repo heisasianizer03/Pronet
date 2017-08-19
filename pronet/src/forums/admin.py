41d1050dfe6e1bf6a842cdbb4558b189791bb69f
heisasianizer03 <paul_nhadz@yahoo.com>
2017-08-19 10:01:38 -0700

    Delete admin.py

--- pronet/src/forums/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Topic, Question, Answer
-
-# Register your models here.
-admin.site.register(Topic)
-admin.site.register(Question)
-admin.site.register(Answer)
